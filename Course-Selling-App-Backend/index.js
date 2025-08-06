ea6ab8da62c842405836f726f905a4e4124035b7
satyamXcode <jaiswalsatyam876@gmail.com>
2025-08-06 18:20:16 +0530

    Added Logic Mongodb database and Script

--- Course-Selling-App-Backend/index.js
+++ Course-Selling-App-Backend/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const { courseRouter } = require('./routes/course');
 const { userRouter } = require('./routes/user');
+const { adminRouter } = require('./routes/admin');
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/course', courseRouter);
 app.use('/api/v1/admin', adminRouter);
 
+async function main(){
+    await mongoose.connect("url");
+    app.listen(3000);
+    console.log("Listening on port 3000");
+}
 
-
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
-});
+main()
--- Course-Selling-App-Backend/routes/admin.js
+++ Course-Selling-App-Backend/routes/admin.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
-
 const adminRouter = Router();
+const { adminModel } = require("../db");
 
 adminRouter.post('/signup', (req, res) => {
     res.json({
@@ -14,6 +14,13 @@ adminRouter.post('/signin', (req, res) => {
     })
 })
 
+adminRouter.post('/course', (req, res) => {
+    res.json({
+        message: "Course"
+    })
+})
+
+
 adminRouter.put('/create', (req, res) => {
     res.json({
         message: "Create Course"
@@ -21,7 +28,7 @@ adminRouter.put('/create', (req, res) => {
 })
 
 
-adminRouter.get('/courses', (req, res) => {
+adminRouter.get('/courses/bulk', (req, res) => {
     res.json({
         message: "Your created courses"
     })
